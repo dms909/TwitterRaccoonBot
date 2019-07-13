e709ca0fc2306bf5c1a1b88a774865ca0b75ec29
Dylan Smith <dylan1999.smith@gmail.com>
2019-07-13 16:41:54 -0500

    Fixed tweepy

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ consumer_key = secret.consumer_key
 consumer_secret = secret.consumer_secret
 access_token = secret.access_token
 access_token_secret = secret.access_token_secret
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
